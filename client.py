ee46f1c731099b469e62a68818c7c4a61dfd725d
Charles Ouyang <ch.k.ouyang@gmail.com>
2016-06-02 15:01:52 +0800

    client.py, model.py updated. Novel class created. test code updated.

--- client.py
+++ client.py
@@ -2,14 +2,16 @@
 """
 import model
 
-# users = [model.User('268339', _following_users=False, _review_list=True),
-#          model.User('161540', _following_users=False, _review_list=True),
-#          model.User('820000', _following_users=False, _review_list=True)]
-users = [model.User('268339', _all=True),
-         model.User('161540', _all=True),
-         model.User('820000', _all=True)]
+users = [model.User('268339', _following_users=False, _review_list=True),
+         model.User('161540', _following_users=False, _review_list=True),
+         model.User('820000', _following_users=False, _review_list=True)]
+users = [model.User('268339'),
+         model.User('161540'),
+         model.User('820000')]
 
 for user in users:
+    user.init(_all=True)
+
     print('')
     print(user.id)
     print(user.name)
@@ -19,3 +21,20 @@ for user in users:
     print(user.following_users)
     print(user.commented_novels)
     print(user.reviews)
+
+novel = model.Novel('n3311bu')
+novel.get_info()
+
+print(novel.id)
+print(novel.title)
+print(novel.user_id)
+print(novel.category)
+print(novel.publication_date)
+print(novel.last_date)
+print(novel.impression_count)
+print(novel.review_count)
+print(novel.bookmark_count)
+print(novel.assessment)
+print(novel.point_writing)
+print(novel.point_story)
+print(novel.chara_count)
